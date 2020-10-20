0e942af5ba2e250ea462752ea86704708a6ec3e7
AnastasiyaKorshina <korshina_nastya@mai.ru>
2020-10-20 18:35:14 +0300

    Initial Commit

---
+++ Animals2010/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animals2010
+//
+//  Created by Анастасия Коршина on 20.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
